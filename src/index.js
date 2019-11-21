bfa42b5d00f72a3fca5d728c30ee624d428db6c9
Dejan Vasic <dejanvasic24@gmail.com>
2019-11-21 14:17:18 +1100

    Adding winston logging

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const { users } = require('./config');
 const { getSecret } = require('./secretFetcher');
 const { processUser } = require('./bankAccountProcessor');
+const logger = require('./logger');
 
 (async function() {
+  logger.info("Service launching");
   for (const userData of users) {
-    const { user, key } = userData;
+    const { key } = userData;
     const userConfig = await getSecret(key);
     await processUser(JSON.parse(userConfig));
   }
--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,27 @@
-const log = (...stuffToLog) => {
-  console.log(...stuffToLog);
-}
+const { createLogger, transports, format } = require('winston');
 
-module.exports = log;
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  defaultMeta: { service: 'banksy' },
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+    // - Write all logs error (and below) to `quick-start-error.log`.
+    //
+    new transports.File({ filename: 'logs-errors.log', level: 'error' }),
+    new transports.File({ filename: 'logs-all.log' }),
+
+    // Everything goes to console as well
+    new transports.Console({ format: format.combine(format.colorize(), format.simple()) })
+  ]
+});
+
+module.exports = logger;
