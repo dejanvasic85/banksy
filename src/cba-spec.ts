a6a2200a99916a7a087916369a7de36e6e6363e0
Dejan Vasic <dejanvasic24@gmail.com>
2019-11-30 18:15:49 +1100

    Fixing the cba tests

--- src/cba-spec.ts
+++ src/cba-spec.ts
@@ -1,19 +1,11 @@
-const { expect } = require('chai');
-
-const {
-  cbaCredentialReader
-} = require('./cba');
-
-const {
-  encrypt
-} = require('./encrypt');
+import { expect } from 'chai';
+import { cbaCredentialReader } from './cba';
+import { encrypt } from './encrypt';
 
 describe('cba', () => {
-
   describe('cbaCredentialsReader', () => {
     const { memberNumber, password } = cbaCredentialReader(encrypt('randomuser|randompassword'));
     expect(memberNumber).to.equal('randomuser');
     expect(password).to.equal('randompassword');
   });
-
 });
