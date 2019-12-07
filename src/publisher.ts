27b35125ebffb4a8b20203d4939b4b9cc35f14a2
Dejan Vasic <dejanvasic24@gmail.com>
2019-12-07 18:32:09 +1100

    Adding bank and account to the sns message

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,21 +1,21 @@
 import { SNS } from 'aws-sdk';
-import { BankTransaction, PublisherConfig } from './types';
+import { BankTransaction, PublisherConfig, TransactionsMessage } from './types';
 import logger from './logger';
 
-export const publish = async (config: PublisherConfig, transactions: BankTransaction[]): Promise<boolean> => {
+export const publish = async (config: PublisherConfig, message: TransactionsMessage): Promise<boolean> => {
   switch (config.type) {
     case 'sns':
-      return await publishToSns(config.address, transactions);
+      return await publishToSns(config.address, message);
     default: 
       throw new Error('Unsupported publisher config');
   }
 };
 
-export const publishToSns = async (topicArn, transactions: BankTransaction[]): Promise<boolean> => {
+export const publishToSns = async (topicArn, message: TransactionsMessage): Promise<boolean> => {
   const sns = new SNS({ apiVersion: '2010-03-31', region: 'ap-southeast-2' });
 
   var params = {
-    Message: JSON.stringify(transactions),
+    Message: JSON.stringify(message),
     TopicArn: topicArn,
   };
 
