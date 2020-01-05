1117621f751f3cc089c8550ba968169941ef7a2c
Dejan Vasic <dvasic@seek.com.au>
2020-01-06 08:38:42 +1100

    Adding types to the mongoose document UserTransactionsModel

--- src/db/userTransactionRepository.ts
+++ src/db/userTransactionRepository.ts
@@ -1,6 +1,6 @@
-import { UserTransactions } from './userTransactions';
+import { createUserTransactions } from './userTransactions';
 import logger from '../logger';
-import { BankTransaction } from '../types';
+import { BankTransaction, UserTransactionsModel } from '../types';
 import * as moment from 'moment';
 
 const clean = str =>
@@ -15,16 +15,17 @@ export const createKey = (date: Date, bankId: string, accountName: string, usern
   return `${dateStr}-${clean(bankId)}-${clean(accountName)}-${clean(username)}`;
 };
 
-export const getTransactions = async ({ date, bankId, accountName, username }) => {
+export const getTransactions = async ({ date, bankId, accountName, username }): Promise<UserTransactionsModel> => {
   const key = createKey(date, bankId, accountName, username);
 
   logger.info(`userTransactionRepository. Fetching transactions for ${key}`);
+  const UserTransactions = await createUserTransactions();
   const todaysTransactions = await UserTransactions.findById(key);
-  
+
   if (todaysTransactions) {
     return todaysTransactions;
   }
-  
+
   const newData = {
     _id: key,
     transactions: [],
@@ -33,7 +34,8 @@ export const getTransactions = async ({ date, bankId, accountName, username }) =
   return await UserTransactions.create(newData);
 };
 
-export const updateTransactions = async (id: string, txns: BankTransaction[]) => {
+export const updateTransactions = async (id: string, txns: BankTransaction[]): Promise<void> => {
+  const UserTransactions = await createUserTransactions();
   await UserTransactions.findByIdAndUpdate(id, {
     $push: {
       transactions: {
